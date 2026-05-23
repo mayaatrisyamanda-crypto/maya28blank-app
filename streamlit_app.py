e2d1ce2064b226717718f7fb07e6f0bdec133172
mayaatrisyamanda-crypto <mayaatrisyamanda@gmail.com>
2026-05-23 10:13:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 
-st.title("🎈 aplikasimaya")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# app.py — baris 13
+st.set_page_config(
+  page_title="kuliah praktisi 2305",
+  layout="centered"
+
+
+# Hirarki teks
+st.title("📊 Dashboard")
+st.header("Laporan Bulanan")
+st.subheader("📈 Monthly Expenses")
+st.caption("Made with ❤️ using Streamlit")
+
+
